980602ddd4de680f030185c7314d88ab6da62aba
Damneet Sambhy <damneet.sambhy@hotmail.com>
2022-03-24 12:33:12 +0930

    Added letter arrays and basic generatePassword function behaviour

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,24 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+const upperCaseLetters = [...Array(26)].map((val,i)=>String.fromCharCode(i + 65))
+const lowerCaseLetters = upperCaseLetters.map(letter => letter.toLowerCase())
+
+function generatePassword(){
+  var passwordLength = Number(window.prompt("Please enter the password length (Minimum 8 characters and no more than 128)"))
+  if(passwordLength<8 || passwordLength>128){
+    window.alert("Please use a password length of minimum 8 characters and maximum 128")
+    passwordLength = Number(window.prompt("Please enter the password length (Minimum 8 characters and no more than 128)"))
+  }
+  
+  var includesLowerCase = window.confirm("Include lower case?")
+  var includesUpperCase = window.confirm("Include upper case?")
+  var includesNumeric = window.confirm("Include numbers?")
+  var includesSpecialCharacters = window.confirm("Include special characters?")
+
+  if(includesLowerCase){
+
+  }
+}
 
 // Write password to the #password input
 function writePassword() {
