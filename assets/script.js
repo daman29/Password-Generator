ec9732eb15146cb8229ae53b1bc03d11e1399b18
Damneet Sambhy <damneet.sambhy@hotmail.com>
2022-03-26 14:11:15 +1030

    pushing changes for ASKBCS help

--- assets/script.js
+++ assets/script.js
@@ -1,15 +1,25 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+
 const upperCaseLetters = [...Array(26)].map((val,i)=>String.fromCharCode(i + 65))
 const lowerCaseLetters = upperCaseLetters.map(letter => letter.toLowerCase())
 const specialCharactersString = " !\"#$%&'()*+,-./:;<=>?@[]^_`{|}~\\"
 const specialCharacters = specialCharactersString.split("")
+const wrongLength = "Please use a password length of minimum 8 characters and maximum 128"
+const lengthPrompt = "Please enter the password length (Minimum 8 characters and no more than 128)"
 
+//Generate password function
 function generatePassword(){
-  var passwordLength = Number(window.prompt("Please enter the password length (Minimum 8 characters and no more than 128)"))
+  var passwordLength = Number(window.prompt(lengthPrompt))
+
+  if (!passwordLength){
+    return
+  }
+
   if(passwordLength<8 || passwordLength>128){
-    window.alert("Please use a password length of minimum 8 characters and maximum 128")
-    passwordLength = Number(window.prompt("Please enter the password length (Minimum 8 characters and no more than 128)"))
+    window.alert(wrongLength)
+    passwordLength = 0
+    generatePassword()
   }
   
   var includesLowerCase = window.confirm("Include lower case?")
@@ -18,13 +28,15 @@ function generatePassword(){
   var includesSpecialCharacters = window.confirm("Include special characters?")
 
   if(!includesLowerCase && !includesUpperCase && !includesNumeric && !includesSpecialCharacters){
-    window.alert("Please select atleast one character type")
+    window.alert("Please select at least one character type")
     generatePassword()
   }
+}
 
-  if(includesLowerCase){
-
-  }
+//Choose random character
+function randomCharacterSelector(type){
+  var randomIndex = Math.floor(Math.random() * type.length)
+  return (type[randomIndex])
 }
 
 // Write password to the #password input
